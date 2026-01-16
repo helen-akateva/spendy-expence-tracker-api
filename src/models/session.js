3041c52b724902a9c68be57cdea73ecdd686ac11
helen-akateva <akateva17@gmail.com>
2026-01-16 18:28:16 +0200

    fixed models

--- src/models/session.js
+++ src/models/session.js
@@ -1,29 +1,17 @@
-
-import { Schema, model } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
 const sessionSchema = new Schema(
   {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    accessToken: {
-      type: String,
-      required: true,
-    },
-    refreshToken: {
-      type: String,
-      required: true,
-    },
-    accessTokenValidUntil: {
-      type: Date,
-      required: true,
-    },
-    refreshTokenValidUntil: {
-      type: Date,
-      required: true,
-    },
-
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 export const Session = model('Session', sessionSchema);
